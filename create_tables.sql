b2e8a10345ba11fc5992406bb034289a0c55e8b6
Daniel Troyo <bollackdtg@gmail.com>
2015-04-02 01:43:24 -0600

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,3 +1,14 @@
 --Scripts de creación & alter de tablas
 
-CRE
+CREATE TABLE Mascotas
+  ID NUMBER(Identity(0,1) Constraint PK_Mascotas PRIMARY KEY (ID),
+  Nombre VARCHAR2(20),
+  Tamaño VARCHAR2(10),
+  Color  VARCHAR2(20),
+  Tel_contacto VARCHAR2(20),
+  Lugar VARCHAR2(20),
+  Nivel_energia VARCHAR2(20),
+  Enfermedades VARCHAR(20), --ANALIZAR
+  Veterinario VARCHAR(20),
+  Fotografia_antes BLOB,
+  
