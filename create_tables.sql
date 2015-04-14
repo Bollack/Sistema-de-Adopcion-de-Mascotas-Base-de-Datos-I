13300f42e79ec29f23c0cb15fadc4f285a66d83d
Cgironalas <cgironalas@gmail.com>
2015-04-14 09:22:19 -0600

    Update create_tables.sql
    Se le agrego a la tabla persona un atributo de fecha de nacimiento, se le borro la referencia a user y esta se le agrego a user la referencia a persona por medio del id. Se considera que no son necesarias las tablas de rescatista y adoptante, se van a meter a comentarios pero hay que considerar borrarlas.
    
    Me parece que en mascota en el atributo estado no deberia tener default. Se cambio el largo de notas a 140.Se cambiaron nivel de energia y espacio recorrido a numeros de 1 a 10 y 1 a 5 respectivamente.
    
    Se agrega la tabla telefono. Hay que agregar la entidad telefono al diagrama.

--- create_tables.sql
+++ create_tables.sql
@@ -10,17 +10,17 @@ CREATE TABLE Mascota --Creado
   Color2  VARCHAR2(20),
   Tel_contacto VARCHAR2(20) CONSTRAINT tel_rescatista_mascota_nn NOT NULL,
   Lugar VARCHAR2(20) CONSTRAINT lugar_mascota_nn NOT NULL,
-  Nivel_energia VARCHAR2(20),
+  Nivel_energia NUMBER CONSTRAINT nivel_energia_check CHECK(-1<Nivel_energia<11),
   Enfermedades VARCHAR2(20), --ANALIZAR
   Veterinario VARCHAR2(20),
   Fotografia_antes BLOB CONSTRAINT foto_antes_mascota_nn NOT NULL,
-  Correo_contacto VARCHAR2(30),
-  Espacio_requerido VARCHAR2(10),
+  Correo_contacto VARCHAR2(30) CONSTRAINT correo_rescatista_mascota_nn NOT NULL,
+  Espacio_requerido NUMBER CONSTRAINT espacio_requerido_check(-1<Espacio_requerido<6),
   Tipo_mascota VARCHAR2(15) CONSTRAINT tipo_mascota_nn NOT NULL, --Foreign Key a Tabla de Tipo
   Raza VARCHAR2(30), ---Foreign Key a Tabla de Raza
   Medicamentos VARCHAR2(20), --ANALIZAR
   Estado VARCHAR2(13) DEFAULT ('En abandono'),
-  Notas VARCHAR2(75),
+  Notas VARCHAR2(140),
   Tratamientos VARCHAR2(25), --ANALIZAR
   Usuario_creacion VARCHAR(20) CONSTRAINT mascota_usuario_creacion_nn NOT NULL,
   Fecha_creacion DATE CONSTRAINT mascota_fecha_creacion_nn NOT NULL,
@@ -80,42 +80,55 @@ ALTER TABLE Mascota
 CREATE TABLE Usuario
 (
   id Number,
-  CONSTRAINT usuario_id_pk PRIMARY KEY(Nombre),
+  CONSTRAINT usuario_pk PRIMARY KEY(id),
+  CONSTRAINT usuario_id_fk FOREIGN KEY (id) REFERENCES Persona(id),
   Nombre VARCHAR2(30),
   CONSTRAINT usuario_nombre_un UNIQUE(nombre),
   Password VARCHAR2(20) CONSTRAINT usuario_password_lenght CHECK(lenght(password>)7)
 );
 
 CREATE TABLE Persona
+(
   id Number,
   CONSTRAINT persona_pk PRIMARY KEY (ID),
   nombre VARCHAR2(20) CONSTRAINT persona_nombre_nn NOT NULL,
   apellido VARCHAR2(20) CONSTRAINT persona_apellido_nn NOT NULL,
   provincia VARCHAR2(10) CONSTRAINT persona_provincia_nn NOT NULL,
-  telefono VARCHAR2(10),
+  --telefono VARCHAR2(10),
   email VARCHAR2(40) CONSTRAINT persona_email_nn NOT NULL,
-  usuario NUMBER CONSTRAINT persona_username_fk FOREIGN KEY (usuario) REFERENCES Usuario(id);
+  fecha_nacimiento DATE CONTRAINT fecha_nacimiento_nn NOT NULL,
+  ---usuario NUMBER CONSTRAINT persona_username_fk FOREIGN KEY (usuario) REFERENCES Usuario(id);
   Usuario_creacion VARCHAR(20) CONSTRAINT mascota_usuario_creacion_nn NOT NULL,
   Fecha_creacion DATE CONSTRAINT mascota_fecha_creacion_nn NOT NULL,
-  Usuario_Modificacion VARCHAR(20)
-  Fecha_Modificacion DATE,
+  Usuario_Modificacion VARCHAR(20),
+  Fecha_Modificacion DATE
+);
+
+----------------------------------------------------------------------------------------------------------------------------
+CREATE TABLE Telefono
+(
+  id_Telefono Number CONSTRAINT telefono_pk PRIMARY KEY (idTelefono),
+  numero Varchar2(10) CONSTRAINT numero_nn NOT NULL, CONSTRAINT numero_uniquie UNIQUE(numero),
+  categoria Varchar(20) CONTSTRAINT categoria_nn NOT NULL,      --- Foreign Key a tabla catalogo (falta hacer)
+  id_Persona Number Constraint id_Persona_fk FOREIGN KEY (id_Persona) REFERENCES Persona(id)
+);
+
+
+--CREATE TABLE Rescatista
+  --idN
+  --persona Number,
+  --CONSTRAINT rescatista_persona_pk PRIMARY KEY 
+  --Usuario_creacion VARCHAR(20) CONSTRAINT mascota_usuario_creacion_nn NOT NULL,
+  --Fecha_creacion DATE CONSTRAINT mascota_fecha_creacion_nn NOT NULL,
+  --Usuario_Modificacion VARCHAR(20)
+  --Fecha_Modificacion DATE,
   
 
-CREATE TABLE Rescatista
-  idN
-  persona Number,
-  CONSTRAINT rescatista_persona_pk PRIMARY KEY 
-  Usuario_creacion VARCHAR(20) CONSTRAINT mascota_usuario_creacion_nn NOT NULL,
-  Fecha_creacion DATE CONSTRAINT mascota_fecha_creacion_nn NOT NULL,
-  Usuario_Modificacion VARCHAR(20)
-  Fecha_Modificacion DATE,
-  
-
-CREATE TABLE Adoptante
-  Usuario_creacion VARCHAR(20) CONSTRAINT mascota_usuario_creacion_nn NOT NULL,
-  Fecha_creacion DATE CONSTRAINT mascota_fecha_creacion_nn NOT NULL,
-  Usuario_Modificacion VARCHAR(20)
-  Fecha_Modificacion DATE,
+--CREATE TABLE Adoptante
+  --Usuario_creacion VARCHAR(20) CONSTRAINT mascota_usuario_creacion_nn NOT NULL,
+  --Fecha_creacion DATE CONSTRAINT mascota_fecha_creacion_nn NOT NULL,
+  --Usuario_Modificacion VARCHAR(20)
+  --Fecha_Modificacion DATE,
 
 
 CREATE TABLE Califica_a
